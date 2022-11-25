ef71493e325f3d5057bd9fe6a40f84785ed75bb0
Jérôme Desboeufs <jerome@livingdata.co>
2022-11-25 14:02:58 +0100

    Add progression

--- worker.js
+++ worker.js
@@ -11,7 +11,7 @@ import pumpify from 'pumpify'
 import {createGeocodeStream} from 'addok-geocode-stream'
 import {validateCsvFromStream, createCsvReadStream} from '@livingdata/tabular-data-helpers'
 
-import {processNext, getInputFileDownloadStream, getProject, setOutputFile, endProcessing} from './lib/models/project.js'
+import {processNext, getInputFileDownloadStream, getProject, setOutputFile, endProcessing, updateProcessing} from './lib/models/project.js'
 
 import {createWriteStream as createGeoJsonWriteStream} from './lib/writers/geojson.js'
 import {createWriteStream as createCsvWriteStream} from './lib/writers/csv.js'
@@ -65,22 +65,59 @@ async function main() {
       console.log(`${projectId} | start processing`)
 
       const project = await getProject(projectId)
+      const {inputFile} = project
 
-      const {geocodeOptions, outputFormat} = project.pipeline
-      const createWriteStream = OUTPUT_FORMATS[outputFormat]
+      const upLimit = pLimit(1)
+
+      /* Validation */
+
+      let totalRows = null
+
+      await upLimit(() => updateProcessing(projectId, {
+        step: 'validating',
+        validationProgress: {readRows: 0, readBytes: 0, totalBytes: inputFile.size}
+      }))
 
       const validationInputStream = await getInputFileDownloadStream(projectId)
 
       await new Promise((resolve, reject) => {
-        validateCsvFromStream(validationInputStream, project.pipeline)
-          .on('error', error => reject(error))
-          .on('complete', () => resolve())
+        const validation = validateCsvFromStream(validationInputStream, project.pipeline)
+
+        validation
+          .on('progress', async progress => {
+            await upLimit(() => updateProcessing(projectId, {
+              validationProgress: {readRows: progress.readRows, readBytes: progress.readBytes, totalBytes: progress.totalBytes}
+            }))
+          })
+          .on('error', async error => {
+            await upLimit(() => updateProcessing(projectId, {
+              validationError: error.message
+            }))
+            reject(new Error('Validation failed'))
+          })
+          .on('complete', async () => {
+            totalRows = validation.readRows
+            await upLimit(() => updateProcessing(projectId, {
+              validationProgress: {readRows: validation.readRows, readBytes: validation.readBytes, totalBytes: validation.totalBytes}
+            }))
+            resolve()
+          })
       })
+
+      /* Geocoding */
+
+      await upLimit(() => updateProcessing(projectId, {
+        step: 'geocoding',
+        geocodingProgress: {readRows: 0, totalRows}
+      }))
 
       const inputFileName = project.inputFile.filename
       const outputFileName = computeOutputFilename(inputFileName || 'result', outputFormat)
 
+      const {geocodeOptions, outputFormat} = project.pipeline
+
       const inputFileStream = await getInputFileDownloadStream(projectId)
+      const createWriteStream = OUTPUT_FORMATS[outputFormat]
 
       const fullGeocodeStream = pumpify(
         inputFileStream,
@@ -89,12 +126,25 @@ async function main() {
           columns: geocodeOptions.q,
           citycode: geocodeOptions.citycode,
           lon: geocodeOptions.lon,
-          lat: geocodeOptions.lat
+          lat: geocodeOptions.lat,
+          async onUnwrap(readRows) {
+            await upLimit(() => updateProcessing(projectId, {
+              geocodingProgress: {readRows, totalRows}
+            }))
+          }
         }),
         createWriteStream()
       )
 
-      await setOutputFile(projectId, outputFileName, fullGeocodeStream)
+      try {
+        await setOutputFile(projectId, outputFileName, fullGeocodeStream)
+      } catch (error) {
+        await upLimit(() => updateProcessing(projectId, {
+          validationError: error.message
+        }))
+        throw new Error('Geocoding failed')
+      }
+
       await endProcessing(projectId)
 
       console.log(`${projectId} | processed successfully`)
